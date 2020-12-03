11042e3d95ebfc48ee93e84ece61955fe7ef98ea
IhnatovychYura <yuraihnatovych111@gmail.com>
2020-12-03 21:10:16 +0200

    React Lesson Two

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            < div >
+            Створити масив man. { id, name, age, wife_id }
+        Створити масив woman. { id, name, age, husband_id }
+
+        Поділити сторінку на 3 колонки, в 1 вивести чоловіків, в 2 жінок.
+            В третю колонку ми виводимо пару ( чоловік + жінка, відповідно до id)
+
+        Компоненти - Mans, Man, Womens, Woman, MarriedCouples, MarriedCouple
+            < /div>
+    );
+    }
 }
 
 export default App;
